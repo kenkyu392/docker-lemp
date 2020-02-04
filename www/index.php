bd7215f3920278a1bf724c86e8151f815b8e4464
kenkyu392 <57144941+kenkyu392@users.noreply.github.com>
2020-02-05 01:23:50 +0900

    Fix format

--- www/index.php
+++ www/index.php
@@ -1,13 +1,12 @@
 <?php
-echo date( 'Y-m-d- H:i:s' ) . '<br>';
+echo date('Y-m-d- H:i:s') . '<br>';
 try {
-    $dns = 'mysql:dbname=' . getenv('MYSQL_DATABASE') . ';host=mysql;charset=utf8mb4';
-    $username = getenv('MYSQL_USER');
-    $password = getenv('MYSQL_PASSWORD');
-    $pdo = new PDO($dns, $username, $password);
+  $dns =
+    'mysql:dbname=' . getenv('MYSQL_DATABASE') . ';host=mysql;charset=utf8mb4';
+  $username = getenv('MYSQL_USER');
+  $password = getenv('MYSQL_PASSWORD');
+  $pdo = new PDO($dns, $username, $password);
 } catch (PDOException $e) {
-    echo 'MySQL (PDO) : ' . $e->getMessage();
-    exit;
+  echo 'MySQL (PDO) : ' . $e->getMessage();
+  exit();
 }
-
-?>
